f12477ca246e75565c353684ae11e2b7d1274701
Mehran Shakerinava <mehranshakerinava@gmail.com>
2023-01-15 15:25:17 -0500

    add new gnn

--- tud_benchmark/gnn_baselines/gnn_architectures.py
+++ tud_benchmark/gnn_baselines/gnn_architectures.py
@@ -5,6 +5,166 @@ from torch_geometric.nn import GINConv, global_mean_pool, JumpingKnowledge
 from torch_geometric.nn import MessagePassing
 from torch_geometric.nn.inits import reset
 
+import torch.nn as nn
+from torch_scatter import scatter
+
+
+# TODO: try having a learnable mixing weight for message passing
+def propagate_messages(x, reduction='none'):
+    # x.shape == (2, E, C)
+    assert x.dim() == 3
+    assert x.shape[0] == 2
+    if reduction == 'none':
+        return torch.cat((x, x.flip(0)), dim=2)
+    elif reduction == 'mean':
+        return (x + x.flip(0)) / 2
+    elif reduction == 'max':
+        return torch.maximum(x, x.flip(0))
+    elif reduction == 'min':
+        return torch.minimum(x, x.flip(0))
+    elif reduction == 'sum':
+        return x + x.flip(0)
+    else:
+        raise NotImplementedError
+
+
+# NOTE: isolated node features will be lost without self-loops
+def expand_features(x, edge_index):
+    # node features to edge features
+    assert x.dim() == 2
+    assert edge_index.dim() == 2
+    assert edge_index.shape[0] == 2
+    return x[edge_index]
+
+
+def contract_features(x, edge_index):
+    # edge features to node features
+    assert x.dim() == 3
+    assert x.shape[0] == 2
+    assert edge_index.dim() == 2
+    assert edge_index.shape[0] == 2
+    return scatter(src=x.view(-1, x.shape[2]), index=edge_index.view(-1), dim=0, reduce='mean')
+
+
+class GraphConv(nn.Module):
+    def __init__(self, in_channels, out_channels, attention=False, key_dim=0):
+        super().__init__()
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.attention = attention
+        self.key_dim = key_dim
+
+        if attention:
+            assert key_dim > 0
+            self.lin_q = nn.Linear(in_channels, key_dim)
+            self.lin_k = nn.Linear(in_channels, key_dim)
+            self.lin_v = nn.Linear(in_channels, out_channels)
+            raise NotImplementedError
+        else:
+            self.lin_on = nn.Linear(in_channels, out_channels)
+            self.lin_off = nn.Linear(in_channels, out_channels)
+
+    # TODO: self-loops, edge types, isolated nodes
+    def forward(self, x, edge_index):
+        n = torch.max(edge_index) + 1
+        e = edge_index.shape[1]
+
+        assert x.shape == (2, e, self.in_channels)
+        assert edge_index.shape == (2, e)
+
+        x_on = self.lin_on(x)
+        x_off = self.lin_off(x)
+
+        assert x_on.shape == (2, e, self.out_channels)
+        assert x_off.shape == (2, e, self.out_channels)
+
+        # TODO: the layer can also be (non-linear) self-attention where every edge-end generates [key, value, query]. This would make the GNN anisotropic.
+        # NOTE: currently, the layer is a linear set-equivariant layer, which is isotropic.
+        # TODO: try adding a learnable/tunable temperature parameter to the softmax of attention
+
+        # NOTE: scatter is highly parallelizable and suitable for GPUs
+        z = scatter(
+            src=x_off.view(-1, self.out_channels),
+            index=edge_index.view(-1),
+            dim=0, reduce='sum')
+        deg = scatter(
+            src=torch.ones_like(x)[..., 0].view(-1),
+            index=edge_index.view(-1),
+            dim=0, reduce='sum')
+
+        assert z.shape == (n, self.out_channels)
+        assert deg.shape == (n,)
+
+        # NOTE: the following is a gather operation which, similar to the scatter operation above, is highly parallelizable and suitable for GPUs
+        x_off = z[edge_index]
+        deg = deg[edge_index]
+        x_out = (x_on + x_off) / deg[..., None]
+
+        assert x_off.shape == (2, e, self.out_channels)
+        assert deg.shape == (2, e)
+        assert x_out.shape == (2, e, self.out_channels)
+
+        return x_out
+
+
+class GraphConvNet(nn.Module):
+    def __init__(self, dataset, num_layers, hidden, act_fn=F.relu, conv_repeat=1, skip_connection=False, reduction='mean'):
+        super().__init__()
+        self.dataset = dataset
+        self.num_layers = num_layers
+        self.hidden = hidden
+        self.act_fn = act_fn
+        self.conv_repeat = conv_repeat
+        self.skip_connection = skip_connection
+        self.reduction = reduction
+        self.conv1 = GraphConv(dataset.num_features + dataset.num_edge_features, hidden)
+        self.convs = nn.ModuleList()
+        for i in range(num_layers - 1):
+            for _ in range(conv_repeat):
+                self.convs.append(GraphConv(hidden * (2 if reduction == 'none' else 1), hidden))
+        self.lin1 = Linear(hidden, hidden)
+        self.lin2 = Linear(hidden, dataset.num_classes)
+
+    def reset_parameters(self):
+        for conv in self.convs:
+            conv.reset_parameters()
+        self.lin1.reset_parameters()
+        self.lin2.reset_parameters()
+
+    def forward(self, data):
+        x, edge_index, batch, edge_attr = data.x, data.edge_index, data.batch, data.edge_attr
+
+        x = expand_features(x, edge_index)
+        # x.shape == (2, E, dataset.num_features)
+
+        if edge_attr is not None:
+            print(edge_attr.shape)
+            assert False
+            # TODO: implement this part when the shape is known
+            # possible implementation for edge_attr.shape == (E, d):
+            # edge_attr = torch.stack((edge_attr, edge_attr), dim=0)
+            # x = torch.cat((x, edge_attr), dim=2)
+
+        # x = propagate_messages(x, reduction='none')
+        x = self.conv1(x, edge_index)
+        x = self.act_fn(x)
+        x = propagate_messages(x, reduction=self.reduction)
+        for conv in self.convs:
+            x_old = x
+            x = conv(x, edge_index)
+            x = self.act_fn(x)
+            # TODO: batchnorm or some kind of layer normalization
+            x = propagate_messages(x, reduction=self.reduction)
+            if self.skip_connection:
+                x[..., :x_old.shape[2]] += x_old
+
+        x = contract_features(x, edge_index)
+        x = global_mean_pool(x, batch)
+        x = self.act_fn(self.lin1(x))
+        x = F.dropout(x, p=0.5, training=self.training)
+        x = self.lin2(x)
+        return F.log_softmax(x, dim=-1)
+
 
 # Taken from https://github.com/rusty1s/pytorch_geometric/blob/master/benchmark/kernel/gin.py.
 class GIN(torch.nn.Module):
---
+++ tud_benchmark/main_gnn_new.py
@@ -0,0 +1,152 @@
+import os
+import io
+import sys
+import json
+import random
+import hashlib
+import argparse
+import builtins
+import subprocess
+import torch
+import numpy as np
+import auxiliarymethods.datasets as dp
+from auxiliarymethods.gnn_evaluation import gnn_evaluation
+from gnn_baselines.gnn_architectures import GraphConvNet
+
+
+def hash_args(args_dict, no_hash):
+    args_dict = {x: args_dict[x] for x in args_dict if x not in no_hash}
+    args_str = json.dumps(args_dict, sort_keys=True, indent=4)
+    args_hash = hashlib.md5(str.encode(args_str)).hexdigest()[:8]
+    return args_hash
+
+
+def main(args):
+    num_reps = 10
+
+    ### Smaller datasets.
+    dataset = [["IMDB-BINARY", False], ["IMDB-MULTI", False], ["NCI1", True], ["PROTEINS", True],
+               ["REDDIT-BINARY", False], ["ENZYMES", True]]
+
+    results = []
+    for d, use_labels in dataset:
+        # Download dataset.
+        dp.get_dataset(d)
+
+        # GraphConvNet, dataset d, layers in [1:6], hidden dimension in {32,64,128}.
+        acc, s_1, s_2 = gnn_evaluation(
+            GraphConvNet, d, [1, 2, 3, 4, 5], [32, 64, 128],
+            max_num_epochs=args.max_num_epochs, batch_size=64, start_lr=0.01, num_repetitions=num_reps, all_std=True,
+            conv_repeat=args.conv_repeat, reduction=args.reduction, activation_fn=args.activation_fn, skip_connection=args.skip_connection
+        )
+        print(d + " " + "GraphConvNet " + str(acc) + " " + str(s_1) + " " + str(s_2))
+        results.append(d + " " + "GraphConvNet " + str(acc) + " " + str(s_1) + " " + str(s_2))
+
+    num_reps = 3
+    print(num_reps)
+
+    ### Midscale datasets.
+    dataset = [["MOLT-4", True, True], ["Yeast", True, True], ["MCF-7", True, True]]
+
+    for d, use_labels, _ in dataset:
+        print(d)
+        dp.get_dataset(d)
+
+        # GraphConvNet, dataset d, 3 layers, hidden dimension in {64}.
+        acc, s_1, s_2 = gnn_evaluation(
+            GraphConvNet, d, [3], [64],
+            max_num_epochs=args.max_num_epochs, batch_size=64, start_lr=0.01, num_repetitions=num_reps, all_std=True,
+            conv_repeat=args.conv_repeat, reduction=args.reduction, activation_fn=args.activation_fn, skip_connection=args.skip_connection
+        )
+        print(d + " " + "GraphConvNet " + str(acc) + " " + str(s_1) + " " + str(s_2))
+        results.append(d + " " + "GraphConvNet " + str(acc) + " " + str(s_1) + " " + str(s_2))
+
+    dataset = [["reddit_threads", False, False],
+               ["github_stargazers", False, False],
+               ]
+
+    for d, use_labels, _ in dataset:
+        print(d)
+        dp.get_dataset(d)
+
+        # GraphConvNet, dataset d, 3 layers, hidden dimension in {64}.
+        acc, s_1, s_2 = gnn_evaluation(
+            GraphConvNet, d, [3], [64], max_num_epochs=args.max_num_epochs,
+            batch_size=64, start_lr=0.01, num_repetitions=num_reps, all_std=True,
+            conv_repeat=args.conv_repeat, reduction=args.reduction, activation_fn=args.activation_fn, skip_connection=args.skip_connection
+        )
+        print(d + " " + "GraphConvNet " + str(acc) + " " + str(s_1) + " " + str(s_2))
+        results.append(d + " " + "GraphConvNet " + str(acc) + " " + str(s_1) + " " + str(s_2))
+
+    for r in results:
+        print(r)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--seed', type=int, default=1)
+    parser.add_argument('--conv-repeat', type=int, default=2)
+    parser.add_argument('--max-num-epochs', type=int, default=200)
+    parser.add_argument('--reduction', type=str, default='mean', choices=['sum', 'mean', 'max', 'min', 'none'])
+    parser.add_argument('--activation-fn', type=str, default='relu')
+    parser.add_argument('--skip-connection', action='store_true', default=False)
+    args = parser.parse_args()
+
+    args_hash = hash_args(vars(args), no_hash=['seed', 'checkpoint_path', 'base_path'])
+
+    FINISH_TEXT = '** finished successfully **'
+    TAG = 'args-%s__seed-%02d' % (args_hash, args.seed)
+
+    os.makedirs('logs', exist_ok=True)
+    LOG_PATH = os.path.join('logs', TAG + '.txt')
+
+    abort = False
+    try:
+        if subprocess.check_output(['tail', '-n', '1', LOG_PATH]).decode('utf-8').strip() == FINISH_TEXT:
+            print('ABORT: experiment has already been performed')
+            abort = True
+    except:
+        pass
+
+    if abort:
+        sys.exit(-1)
+
+    LOG_STR = io.StringIO()
+    LOG_FILE = open(LOG_PATH, 'w')
+
+    old_print = print
+
+    def new_print(*args, **kwargs):
+        kwargs['flush'] = True
+        old_print(*args, **kwargs)
+        kwargs['file'] = LOG_STR
+        old_print(*args, **kwargs)
+        kwargs['file'] = LOG_FILE
+        old_print(*args, **kwargs)
+    
+    builtins.print = new_print
+
+    print('writing log to `%s`' % LOG_PATH)
+
+    print('───────────── machine info ─────────────')
+    print(subprocess.check_output(['uname', '-a']).decode('utf-8').strip())
+    print(subprocess.check_output(['lscpu']).decode('utf-8').strip())
+    print(subprocess.check_output(['nvidia-smi']).decode('utf-8').strip())
+    print('────────────────────────────────────────')
+
+    print(subprocess.check_output(['pip', 'freeze']).decode('utf-8').strip())
+    print('args = %s' % json.dumps(vars(args), sort_keys=True, indent=4))
+
+
+    def set_seed(seed):
+        random.seed(seed, version=2)
+        np.random.seed(random.randint(0, 2**32))
+        torch.manual_seed(random.randint(0, 2**32))
+        torch.backends.cudnn.deterministic = True
+        torch.backends.cudnn.benchmark = False
+
+
+    # ensure reproducibility (?)
+    set_seed(args.seed)
+
+    main(args)
